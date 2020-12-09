81a7fd0da82216cf609b167d054275f7ffcc2b4a
Alexis Evelyn <alexis.a.evelyn@gmail.com>
2020-12-09 00:22:55 -0500

    Forgot To Fix Requirements File

--- archiver/config.py
+++ archiver/config.py
@@ -6,7 +6,7 @@ import os
 WORKING_DIRECTORY: str = "working"
 
 # Dolt Repo Vars
-ARCHIVE_TWEETS_REPO_URL: str = "alexis-evelyn/test"
+ARCHIVE_TWEETS_REPO_URL: str = "alexis-evelyn/presidential-tweets"
 ARCHIVE_TWEETS_REPO_PATH: str = os.path.join(WORKING_DIRECTORY, "presidential-tweets")
 ARCHIVE_TWEETS_TABLE: str = "trump"
 ARCHIVE_TWEETS_COMMIT_MESSAGE: str = "Automated Tweet Update"
