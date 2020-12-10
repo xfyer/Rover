e64091dacf137d06787102f3faa7e87af75be91d
Alexis Evelyn <alexis.a.evelyn@gmail.com>
2020-12-10 07:55:17 -0500

    Added Metadata To The Site

--- ReadMe.md
+++ ReadMe.md
@@ -24,6 +24,44 @@ pip3 install -r requirements.txt
 ### RPi
 
 ```bash
+# Install Python 3.9
+cd /usr/src
+
+# Download and Extract Python 3.9
+sudo wget https://www.python.org/ftp/python/3.9.1/Python-3.9.1.tgz
+sudo tar xvf Python-3.9.1.tgz
+
+# Install Build Tools
+sudo apt-get update
+sudo apt-get install -y build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev
+
+# Build Python 3.9
+cd Python-3.9.1
+sudo ./configure --enable-optimizations
+sudo make altinstall
+
+# Symlink Python (You May Need To Add /usr/local/bin/ To Your Executable Path)
+sudo ln -s /usr/local/bin/python3.9 /usr/local/bin/python
+sudo ln -s /usr/local/bin/python3.9 /usr/local/bin/python3
+
+# OPTIONAL (Disable Built In Python)
+sudo chmod -x /usr/bin/python
+sudo chmod -x /usr/bin/python3
+
+# Install Pip, Wheel, and SetupTools
+python3 -m pip install --upgrade pip setuptools wheel
+
+# Install postgres-dev For psycopg2
+sudo apt-get install libpq-dev
+
+# You may need to specify --no-cache-dir after install
+pip3 install -r requirements.txt
+```
+
+### Deprecated RPI Instructions (Due To 3.7 Being The Latest Release and Requirement For 3.9+)
+
+```bash
+# Install Missing Libraries
 sudo apt-get install libfreetype6-dev libopenjp2-7 libtiff5
 
 # You may need to specify --no-cache-dir after install
--- rover/server/server.py
+++ rover/server/server.py
@@ -59,12 +59,44 @@ class RequestHandler(BaseHTTPRequestHandler):
         return
 
     def do_GET(self):
+        # TODO: Add CSS and Hosting Own Images Support
+
+        # Site Data
+        site_title: str = "Rover"
+        site_icon: str = "https://raw.githubusercontent.com/alexis-evelyn/Rover/master/rover/server/images/Rover.png"
+
+        # Twitter Metadata
+        twitter_title: str = site_title
+        twitter_description: str = "Future Analysis Website Here!!!"
+        twitter_icon: str = site_icon
+
+        # Current Time
+        current_time: str = f"{datetime.now().astimezone(tz=pytz.UTC):%A, %B, %d %Y at %H:%M:%S.%f %z}"
+        background_color: str = "#000000"
+        text_color: str = "#ffffff"
+
+        # HTTP Headers
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(bytes("<html><head><title>Testing</title></head>", "utf-8"))
-        self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-        self.wfile.write(bytes("<p>Current Time: {current_time}</p>".format(current_time=f"{datetime.now().astimezone(tz=pytz.UTC):%A, %B, %d %Y at %H:%M:%S.%f %z}"), "utf-8"))
+
+        # Header
+        self.wfile.write(bytes("<html><head>", "utf-8"))
+        self.wfile.write(bytes(f"<title>{site_title}</title>", "utf-8"))
+        self.wfile.write(bytes(f"<link rel=\"icon\" type=\"image/png\" href=\"{site_icon}\">", "utf-8"))
+
+        # Twitter Cards - https://medium.com/@dinojoaocosta/how-to-make-twitter-preview-your-website-links-5b20db98ac4f
+        self.wfile.write(bytes("<meta name=\"twitter:card\" content=\"summary\" />", "utf-8"))
+        self.wfile.write(bytes("<meta name=\"twitter:site\" content=\"@AlexisEvelyn42\" />", "utf-8"))
+        self.wfile.write(bytes(f"<meta name=\"twitter:title\" content=\"{twitter_title}\" />", "utf-8"))
+        self.wfile.write(bytes(f"<meta name=\"twitter:description\" content=\"{twitter_description}\" />", "utf-8"))
+        self.wfile.write(bytes(f"<meta name=\"twitter:image\" content=\"{twitter_icon}\" />", "utf-8"))
+
+        self.wfile.write(bytes("</head>", "utf-8"))
+
+        # Body
+        self.wfile.write(bytes(f"<body text=\"{text_color}\" bgcolor=\"{background_color}\">", "utf-8"))
+        self.wfile.write(bytes(f"<p>Request: {self.path}</p>", "utf-8"))
+        self.wfile.write(bytes(f"<p>Current Time: {current_time}</p>", "utf-8"))
         self.wfile.write(bytes("<h1>Please Visit Me On <a href=\"https://twitter.com/DigitalRoverDog\">Twitter</a> For The Currently Implemented Features!!!</h1>" % self.path, "utf-8"))
         self.wfile.write(bytes("</body></html>", "utf-8"))
