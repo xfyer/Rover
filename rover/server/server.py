08649af794c8f8fe704576324879313bb7cf140b
Alexis Evelyn <alexis.a.evelyn@gmail.com>
2020-12-10 15:27:11 -0500

    Added Manifest

--- rover/server/server.py
+++ rover/server/server.py
@@ -52,23 +52,35 @@ class WebServer(threading.Thread):
 
 class RequestHandler(BaseHTTPRequestHandler):
     def __init__(self, request: bytes, client_address: Tuple[str, int], server: socketserver.BaseServer):
+        self.logger: logging.Logger = get_logger(__name__)
+        self.INFO_QUIET: int = main_config.INFO_QUIET
+        self.VERBOSE: int = main_config.VERBOSE
+
         super().__init__(request, client_address, server)
 
     def log_message(self, log_format, *args):
-        return
+        self.logger.log(logging.DEBUG, log_format % args)
 
     def do_GET(self):
-        if self.path.lower().startswith("/api/"):
-            api.handle_api(self=self)
-        elif self.path.lower() == "/robots.txt":
-            handler.load_file(self=self, path="rover/server/web/other/robots.txt", mime_type="text/plain")
-        elif self.path.lower() == "/favicon.ico":
-            handler.load_404_page(self=self)
-        elif self.path.lower() == "/images/rover.png":
-            handler.load_file(self=self, path="rover/server/web/images/Rover.png", mime_type="image/png")
-        elif self.path.lower() == "/images/rover.svg":
-            handler.load_file(self=self, path="rover/server/web/images/Rover.svg", mime_type="image/svg+xml")
-        elif self.path.lower() == "/css/stylesheet.css":
-            handler.load_file(self=self, path="rover/server/web/css/stylesheet.css", mime_type="text/css")
-        else:
-            handler.load_page(self=self)
+        try:
+            if self.path.lower().startswith("/api/"):
+                api.handle_api(self=self)
+            elif self.path.lower() == "/manifest.webmanifest":
+                handler.load_file(self=self, path="rover/server/web/other/manifest.json", mime_type="application/manifest+json")
+            elif self.path.lower() == "/robots.txt":
+                handler.load_file(self=self, path="rover/server/web/other/robots.txt", mime_type="text/plain")
+            elif self.path.lower() == "/favicon.ico":
+                handler.load_404_page(self=self)
+            elif self.path.lower() == "/images/rover.png":
+                handler.load_file(self=self, path="rover/server/web/images/Rover.png", mime_type="image/png")
+            elif self.path.lower() == "/images/rover.svg":
+                handler.load_file(self=self, path="rover/server/web/images/Rover.svg", mime_type="image/svg+xml")
+            elif self.path.lower() == "/css/stylesheet.css":
+                handler.load_file(self=self, path="rover/server/web/css/stylesheet.css", mime_type="text/css")
+            else:
+                handler.load_page(self=self)
+        except BrokenPipeError as e:
+            self.logger.debug("{ip_address} Requested {page_url}: {error_message}".format(ip_address=self.address_string(), page_url=self.path, error_message=e))
+
+    def version_string(self):
+        return "Rover"
